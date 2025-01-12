f7d6d20e41a2237b745dd27edbd04409384502dc
Gen-0-cide <156652578+Gen-0-cide@users.noreply.github.com>
2025-01-12 17:40:57 +0300

    Update urls.py

--- Pereval_DRF/Pereval_DRF/urls.py
+++ Pereval_DRF/Pereval_DRF/urls.py
@@ -17,11 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('app_main.urls')),
+    path('api/v1/', include('app_main.urls')),  # Подключение вашего приложения
     path("schema/", SpectacularAPIView.as_view(), name="schema"),
-    path("", SpectacularSwaggerView.as_view(template_name="swagger-ui.html", url_name="schema"), name="swagger-ui",),
+    path("", SpectacularSwaggerView.as_view(template_name="swagger-ui.html", url_name="schema"), name="swagger-ui"),
 ]
