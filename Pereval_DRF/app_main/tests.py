ea6fee7bf9a070f24bb570acb53dd23ff665e905
Gen-0-cide <156652578+Gen-0-cide@users.noreply.github.com>
2025-01-15 12:48:45 +0300

    Update tests.py

--- Pereval_DRF/app_main/tests.py
+++ Pereval_DRF/app_main/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.urls import reverse
 from django.contrib.auth.models import User
 from .models import PerevalUser , Coords, PerevalAdd, Images
 
@@ -99,3 +100,47 @@ class ImagesModelTest(TestCase):
 
     def test_str_method(self):
         self.assertEqual(str(self.image), f"id: {self.image.pk}, title: {self.image.title}")
+
+        
+#class AuthEmailPerevalAPITest(TestCase):
+#    def setUp(self):
+#       self.pereval_user = PerevalUser .objects.create(
+#            user=self.user,
+#            fam='Иванов',
+#            name='Иван',
+#            email='ivanov@example.com',
+#            otc='Иванович',
+#            phone='1234567890'
+#        )
+#        self.coords = Coords.objects.create(latitude=45.0, longitude=90.0, height=1000)
+#        self.pereval_add = PerevalAdd.objects.create(
+#            status=PerevalAdd.NEW,
+#            beauty_title='Прекрасный перевал',
+#            title='Перевал 1',
+#            other_titles='Перевал 1',
+#            connect='Описание перевала',
+#            user=self.pereval_user,
+#            coords=self.coords
+#        )
+#
+#    def test_get_perevals_by_email_success(self):
+#        """Тест успешного получения перевалов по email."""
+#        url = reverse('auth-email-pereval')
+#        response = self.client.get(url, {'user__email': 'ivanov@example.com'})
+#        self.assertEqual(response.status_code, status.HTTP_200_OK)
+#        self.assertIn('data', response.data)
+#        self.assertEqual(len(response.data['data']), 1)
+#
+#    def test_get_perevals_by_email_not_found(self):
+#        """Тест получения перевалов по email, когда email не существует."""
+#        url = reverse('auth-email-pereval')
+#        response = self.client.get(url, {'user__email': 'nonexistent@example.com'})
+#        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+#        self.assertEqual(response.data['message'], 'Нет записей от email = nonexistent@example.com')
+#
+#    def test_get_perevals_by_email_no_email(self):
+#        """Тест получения перевалов без указания email."""
+#        url = reverse('auth-email-pereval')
+#        response = self.client.get(url)
+#        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+#        self.assertEqual(response.data['message'], 'Email не указан')
